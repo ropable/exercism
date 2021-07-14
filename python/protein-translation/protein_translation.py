f76644d2d01d8222553755048756b966693ca925
Ashley Felton <ashley@ropable.com>
2021-07-14 14:28:37 +0800

    Nicer protein translation solution.

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,23 +1,31 @@
 def proteins(strand):
     seq = []
+    codon_map = {
+        "AUG": "Methionine",
+        "UUU": "Phenylalanine",
+        "UUC": "Phenylalanine",
+        "UUA": "Leucine",
+        "UUG": "Leucine",
+        "UCU": "Serine",
+        "UCC": "Serine",
+        "UCA": "Serine",
+        "UCG": "Serine",
+        "UAU": "Tyrosine",
+        "UAC": "Tyrosine",
+        "UGU": "Cysteine",
+        "UGC": "Cysteine",
+        "UGG": "Tryptophan",
+        "UAA": False,
+        "UAG": False,
+        "UGA": False,
+    }
 
     while strand:
         codon, strand = strand[0:3], strand[3:]
-        if codon in ["AUG"]:
-            seq.append("Methionine")
-        elif codon in ["UUU", "UUC"]:
-            seq.append("Phenylalanine")
-        elif codon in ["UUA", "UUG"]:
-            seq.append("Leucine")
-        elif codon in ["UCU", "UCC", "UCA", "UCG"]:
-            seq.append("Serine")
-        elif codon in ["UAU", "UAC"]:
-            seq.append("Tyrosine")
-        elif codon in ["UGU", "UGC"]:
-            seq.append("Cysteine")
-        elif codon in ["UGG"]:
-            seq.append("Tryptophan")
-        elif codon in ["UAA", "UAG", "UGA"]:   # STOP
-            strand = ""
+        if codon in codon_map:
+            if codon_map[codon]:
+                seq.append(codon_map[codon])
+            else:
+                strand = ""
 
     return seq
