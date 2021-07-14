fab9c2e6a7482734c1d0d064af24e2384359e4d6
Ashley Felton <ashley@ropable.com>
2021-07-14 08:39:24 +0800

    Update two_fer.py per mentor suggestion.

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,5 +1,3 @@
-def two_fer(name=None):
-    if name:
-        return "One for {}, one for me.".format(name)
-    else:
-        return "One for you, one for me."
+def two_fer(name="you"):
+
+    return f"One for {name}, one for me."
