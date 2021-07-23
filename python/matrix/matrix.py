4ab36279e00055bf999ca6e9b8b767231256edcd
Ashley Felton <ashley@ropable.com>
2021-07-23 13:01:57 +0800

    Final update to matrix solution.

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,13 +1,10 @@
-from typing import Text, List
-
-
 class Matrix:
-    def __init__(self, matrix_string: Text):
+    def __init__(self, matrix_string: str):
         # Split matrix_string into lines, split lines on whitespace and cast elements as integers.
         self.matrix = [[int(j) for j in i.split()] for i in matrix_string.splitlines()]
 
-    def row(self, index: int) -> List:
+    def row(self, index: int) -> list[int]:
         return self.matrix[index - 1]
 
-    def column(self, index: int) -> List:
+    def column(self, index: int) -> list[int]:
         return [i[index - 1] for i in self.matrix]
